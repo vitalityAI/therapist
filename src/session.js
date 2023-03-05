c9ad7424e203c4e97719ebaf4e0f8c5ac78dd40a
Claeb101 <calebarulandu@gmail.com>
2023-03-04 20:43:54 -0500

    session lib funcs

--- src/session.js
+++ src/session.js
@@ -1,5 +1,75 @@
-import { db } from "./db";
+import { Role } from "@prisma/client";
+import { db } from "./db.js";
 
-const session = () => {
-  db.session.create({})
+export const createSession = async (callId, callerPhone) => {
+  return await db.session.create({
+    data: {
+      callId: callId,
+      callerPhone: callerPhone
+    }
+  })
 }
+
+export const findSessionByCallId = async (callId) => {
+  return await db.session.findUnique({
+    where: {
+      callId: callId
+    }
+  })
+}
+
+export const transferSession = async (callId, receiverPhone) => {
+  return await db.session.update({
+    where: {
+      callId: callId
+    },
+    data: {
+      transferedAt: new Date(),
+      receiverPhone: receiverPhone
+    }
+  })
+}
+
+export const endSession = async (callId) => {
+  return await db.session.update({
+    where: {
+      callId: callId
+    },
+    data: {
+      endedAt: new Date()
+    }
+  })
+}
+
+export const addMessage = async (callId, role, content) => {
+  const session = await findSessionByCallId(callId)
+  return await db.message.create({
+    data: {
+      sessionId: session.id,
+      role: role,
+      content: content
+    }
+  })
+}
+
+export const getMessages = async (callId) => {
+  return await db.message.findMany({
+    where: {
+      session: {
+        callId: callId
+      }
+    },
+    orderBy: [
+      {
+        createdAt: 'asc'
+      }
+    ]
+  })
+}
+
+// const main = async () => {
+//   // await addMessage("ofijse", Role.USER, "wefoij")
+//   console.log(await getMessages("ofijse"))
+// }
+
+// main()
